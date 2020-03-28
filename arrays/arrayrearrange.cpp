b59c347d437d9db8556c00b3d0e3721b272729c0
VivanVatsa <VivanVatsa@users.noreply.github.com>
2020-03-28 10:26:06 +0530

    Update arrayrearrange.cpp

--- arrays/arrayrearrange.cpp
+++ arrays/arrayrearrange.cpp
@@ -36,3 +36,26 @@ int main()
     for (int i = 0; i < n; i++)
     std::cout << ar[i] << " "<< std::endl;
 }
+
+/* alternate method called swap method with complexity of O(n)
+
+#include<stdio.h> 
+
+int main() 
+{ 
+	int arr[] = {-1, -1, 6, 1, 9, 3, 2, -1, 4, -1}; 
+	int n = sizeof(arr)/sizeof(arr[0]); 
+	for (int i = 0; i < n;) 
+	{ 
+		if (arr[i] >= 0 && arr[i] != i) 
+		arr[arr[i]] = (arr[arr[i]] + arr[i]) - 
+						(arr[i] = arr[arr[i]]); 
+		else
+			i++; 
+	} 
+	for(int i = 0; i < n; i++) 
+		printf("%d ",arr[i]); 
+		return 0; 
+} 
+
+*/
