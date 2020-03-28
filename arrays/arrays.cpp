7980b47c3337053b2af40ad603bc3113c5f02628
VivanVatsa <VivanVatsa@users.noreply.github.com>
2020-03-28 10:21:34 +0530

    Create arrays.cpp

---
+++ arrays/arrays.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+// arrays
+
+
+using namespace std;
+
+int main(){
+    
+}
+
+/* 
+
+
+char ch = 'A';
+    int val = (int)ch;
+
+    cout << val << endl;
+
+    bool b = false;
+    //cout << b;
+    int x = (int)b;
+    cout << x << endl;
+
+    int y = 70;
+    char w = (char)y;
+    cout << w << endl;
+    
+    return 0;
+
+char ch = 'A';
+cout << ch + 1 << endl;
+
+ch += 1; // ch=ch+1
+
+cout << ch;
+return 0;
+
+char ch = 'A';
+    int val = (int)ch;
+
+    cout << val << endl;
+
+    bool b = false;
+    //cout << b;
+    int x = (int)b;
+    cout << x << endl;
+    return 0;
+*/
+
+/* using namespace std;
+
+int main(){
+
+    int ar[5], i;
+    cout << "size of the integer" << sizeof(ar[5]) << endl;
+
+    for (i = 0; i< 5; i++){
+        cout << "address of array ar[" << i << "] is" << &ar[i] << "\n";
+    }
+
+    return 0;
+
+}
+*/
+
+//typecasting
