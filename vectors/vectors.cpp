b42fd0581c83ac0c1361f2eaa23132785b39ac0d
VivanVatsa <VivanVatsa@users.noreply.github.com>
2020-03-30 17:57:06 +0530

    Update vectors.cpp

--- vectors/vectors.cpp
+++ vectors/vectors.cpp
@@ -1,166 +1,17 @@
 #include <bits/stdc++.h>
+#include <iostream>
 #include <vector>
-
-using namespace std;
-
-int main()
-{
-    // Assign vector
-    vector<int> v;
-
-    // fill the array with 10 five times
-    v.assign(5, 10);
-
-    cout << "The vector elements are: ";
-    for (int i = 0; i < v.size(); i++)
-        cout << v[i] << " ";
-
-    // inserts 15 to the last position
-    v.push_back(15);
-    int n = v.size();
-    cout << "\nThe last element is: " << v[n - 1];
-
-    // removes last element
-    v.pop_back();
-
-    // prints the vector
-    cout << "\nThe vector elements are: ";
-    for (int i = 0; i < v.size(); i++)
-        cout << v[i] << " ";
-
-    // inserts 5 at the beginning
-    v.insert(v.begin(), 5);
-
-    cout << "\nThe first element is: " << v[0];
-
-    // removes the first element
-    v.erase(v.begin());
-
-    cout << "\nThe first element is: " << v[0];
-
-    // inserts at the beginning
-    v.emplace(v.begin(), 5);
-    cout << "\nThe first element is: " << v[0];
-
-    // Inserts 20 at the end
-    v.emplace_back(20);
-    n = v.size();
-    cout << "\nThe last element is: " << v[n - 1];
-
-    // erases the vector       
-    v.clear();       
-    cout << "\nVector size after erase(): " << v.size();       
-
-    // two vector to perform swap
-    vector<int> v1, v2;
-    v1.push_back(1);
-    v1.push_back(2);
-    v2.push_back(3);
-    v2.push_back(4);
-
-    cout << "\n\nVector 1: ";
-    for (int i = 0; i < v1.size(); i++)
-        cout << v1[i] << " ";
-
-    cout << "\nVector 2: ";
-    for (int i = 0; i < v2.size(); i++)
-        cout << v2[i] << " ";
-
-    // Swaps v1 and v2
-    v1.swap(v2);
-
-    cout << "\nAfter Swap \nVector 1: ";
-    for (int i = 0; i < v1.size(); i++)
-        cout << v1[i] << " ";
-
-    cout << "\nVector 2: ";
-    for (int i = 0; i < v2.size(); i++)
-        cout << v2[i] << " ";
-
-    return 0;
-
-}
-
-
-
-
-
-
-
-
-
-/* #include <iostream>
-#include <vector>
-
-
-
-
-
-
-
-
-
-
-
-
-
 using namespace std;
 
 int main(){
-        vector<int> g1;
+    vector<int> g1;
 
-        for (int i = 1; i <= 5; i++)
-            g1.push_back(i);
+    for (int i = 0; i < 5; i++)
+        g1.push_back(i);
 
-        cout << "Size : " << g1.size();
-        cout << "\nCapacity : " << g1.capacity();
-        cout << "\nMax_Size : " << g1.max_size();
+    cout << "output of the begin and the end is:-" << endl;
+    for (auto i = g1.begin(); i!=g1.end(); ++i)
+        cout << *i << endl;
 
-        g1.resize(4);
-
-        cout << "\nSize : " << g1.size();
-
-        if (g1.empty() == false)
-            cout << "\nVector is not empty";
-        else
-            cout << "\nVector is empty";
-
-        g1.shrink_to_fit();
-        cout << "\nVector elements are: ";
-        for (auto it = g1.begin(); it != g1.end(); it++)
-            cout << *it << " ";
-
-        return 0;
-    }
-    */
-
-
-    /* using namespace std;
-
-    int main()
-    {
-
-        vector<int> g1;
-
-        for (int i = 0; i <= 5; i++)
-            g1.push_back(i);
-
-        cout << "output of vector begin & end" << endl;
-        for (auto i = g1.begin(); i ! = g1.end(); ++i)
-            cout << *i << " ";
-
-        cout << "output of vector cbegin & cend" << endl;
-        for (auto i = g1.cbegin(); i != g1.cend; ++i)
-            cout << *i << " ";
-
-        cout << "output of vector begin & end" << endl;
-        for (auto ir = g1.begin(); ir ! = g1.end(); ++ir)
-            cout << *ir << " ";
-
-        cout << "output of vector begin & end" << endl;
-        for (auto ir = g1.begin(); ir ! = g1.end(); ++ir)
-            cout << *ir << " ";
-
-        return 0;
-    }
-    */
+    
+}
