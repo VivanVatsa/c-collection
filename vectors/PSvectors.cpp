470658b750c51e7d836bce0d6b7ed32def43be94
VivanVatsa <VivanVatsa@users.noreply.github.com>
2020-04-05 20:43:46 +0530

    Create PSvectors.cpp

---
+++ vectors/PSvectors.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(){
+    vector<int> numbers;
+    numbers= {0,1,2};
+    numbers.push_back(-3);
+    number[0] = 4;
+    int num = numbers[2];
+
+    for(int i: numbers){
+        cout << i << endl;
+
+    }
+}
