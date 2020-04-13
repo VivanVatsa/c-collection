eaa9751ed15980b1d2843453420281a24645aea4
VivanVatsa <VivanVatsa@users.noreply.github.com>
2020-04-13 17:21:53 +0530

    Create arraykadanealgo.cpp

---
+++ arrays/arraykadanealgo.cpp
@@ -0,0 +1,17 @@
+// kadane's algorithm 
+/*
+Initialize:
+    max_so_far = 0
+    max_ending_here = 0
+
+Loop for each element of the array
+  (a) max_ending_here = max_ending_here + a[i]
+  (b) if(max_ending_here < 0)
+            max_ending_here = 0
+  (c) if(max_so_far < max_ending_here)
+            max_so_far = max_ending_here
+return max_so_far
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
