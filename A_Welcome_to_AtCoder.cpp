e6635343483e2cf6a90d3a3be61ed66326cd5225
VivanVatsa <VivanVatsa@users.noreply.github.com>
2020-05-14 19:09:49 +0530

    Create A_Welcome_to_AtCoder.cpp

---
+++ A_Welcome_to_AtCoder.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    int a, b, c;
+    cin >> a;
+    cin >> b >> c;
+    string s;
+    // getline(cin, s);
+    cin >> s;
+    int sum = a + b + c;
+    cout << "\n"
+         << sum << " " << s;
+
+         return 0;
+}
