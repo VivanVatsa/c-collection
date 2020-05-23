0b3fecfabc124c445a65cc97eacadded3c665fbd
VivanVatsa <VivanVatsa@users.noreply.github.com>
2020-05-23 15:58:39 +0530

    circular_linked_list travesal in C++ lang

--- linked_list/circular_linked_list/cll1.cpp
+++ linked_list/circular_linked_list/cll1.cpp
@@ -39,4 +39,53 @@ class node{
 // creating the functions
 
 //insetion func
-void pushValue(node **head_ref, int data)
+void pushValue(node **head_ref, int data){
+
+    node *ptr1 = new node();
+    node *temp = *head_ref;
+
+    ptr1->data = data;
+    ptr1->next = *head_ref;
+
+    //here too will check
+    // if linked list is not NULL then set the next of the last node
+
+    if(*head_ref!=NULL){
+        while (temp->next != *head_ref) // loop will end if only found that the pointer is again on the start
+            temp = temp->next;
+        temp->next = ptr1;
+
+    }
+
+    else
+        ptr1->next = ptr1;
+
+    *head_ref = ptr1;
+
+}
+
+void printList(node *head){
+    node *temp = head;
+    if(head !=NULL){
+        do{
+            cout << temp->data << " ";
+            temp = temp->next;
+        } while (temp != NULL);
+
+    }
+}
+
+int main(){
+    node *head = NULL;
+
+    /* Created linked list will be 11->2->56->12 */
+    pushValue(&head, 12);
+    pushValue(&head, 56);
+    pushValue(&head, 2);
+    pushValue(&head, 11);
+
+    cout << "Contents of Circular Linked List\n ";
+    printList(head);
+
+    return 0;
+}
