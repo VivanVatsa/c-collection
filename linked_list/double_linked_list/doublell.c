a7964f0833f2f7816da9e5d854042f9345a12a9e
VivanVatsa <VivanVatsa@users.noreply.github.com>
2020-06-11 17:22:24 +0530

    starting on with new data structure doubly ll

---
+++ linked_list/double_linked_list/doublell.c
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+//in double linked list we add extra pointer previous pointer
+struct node{
+    int data;
+    struct node *next;
+    struct node *prev;
+};
+
+void push(struct ndoe **head_ref, struct node **previous)
+int main(void){
+
+}
