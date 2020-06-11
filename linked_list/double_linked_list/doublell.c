cf7d81d5ac0cd4283af9c8cacdbe23385b2da660
VivanVatsa <VivanVatsa@users.noreply.github.com>
2020-06-11 22:19:36 +0530

    few code for double linked_list

--- linked_list/double_linked_list/doublell.c
+++ linked_list/double_linked_list/doublell.c
@@ -6,7 +6,28 @@ struct node{
     struct node *prev;
 };
 
-void push(struct ndoe **head_ref, struct node **previous)
+
+// insertion at the start of the node
+void push(struct node **head_ref,int newData){
+    //allocate the new node
+    struct node *new_node = (struct node*)malloc(sizeof(struct node));
+
+    // insert the data
+    new_node->data = newData;
+
+    // now assigning both pointers their respective values
+    new_node->next = *head_ref;
+    new_node->prev = nullptr;
+
+    /* 4. change prev of head node to new node */
+        if((*head_ref)!=NULL)
+        (*head_ref)->prev=new_node;
+
+    // move the head to point to the new node
+        (*head_ref) = new_node;
+}
+
+
 int main(void){
 
 }
