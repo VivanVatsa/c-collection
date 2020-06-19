e60120e74812ec1f71af6b32013aa9941af41355
VivanVatsa <VivanVatsa@users.noreply.github.com>
2020-06-19 23:19:57 +0530

    stack in python

---
+++ misc/stack.py
@@ -0,0 +1,21 @@
+stack = []
+stack.append('A')
+stack.append('P')
+stack.append('P')
+stack.append('L')
+stack.append('E')
+
+print('Initial Stack')
+print(stack)
+
+print('elements popped of stack')
+print(stack.pop())
+print(stack.pop())
+print(stack.pop())
+print(stack.pop())
+
+print('stack after pop')
+print(stack)
+
+
+
