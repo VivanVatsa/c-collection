7a116d71adbc548ac0e4270e18c18220580b70e9
VivanVatsa <VivanVatsa@users.noreply.github.com>
2020-06-23 22:34:51 +0530

    magician algo in C++ STL

---
+++ C++STL/algorithms/magiciansalgo.cpp
@@ -0,0 +1,21 @@
+// all operations in VECTOR
+
+// non manipulating algorithm
+
+#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <numeric>
+
+using namespace std;
+
+int main(){
+
+    // init vector with array
+    int ar[] = {10, 20, 5, 23, 42, 15};
+    int n = sizeof(ar) / sizeof(ar[0]);
+    vector<int> vect(ar, ar + n);
+
+    cout << "vector is: "
+}
+
