51a19a3d862317a6cd60db1c2cb6134f0f803bd7
VivanVatsa <VivanVatsa@users.noreply.github.com>
2020-06-27 14:10:58 +0530

    Update infix2postfixC.c

--- stack/C-stack/infix2postfixC.c
+++ stack/C-stack/infix2postfixC.c
@@ -35,3 +35,24 @@ struct Stack *createStack(unsigned capacity){
 
     return stack;
 }
+
+// check if stack is empty
+int isEmpty(struct Stack *stack){
+    return stack->top == -1;
+}
+
+//to return the top element in the stack
+int peek(struct Stack *stack){
+    return stack->array[stack->top];
+}
+
+char pop(struct Stack *stack)
+{
+    if (!isEmpty(stack))
+        return stack->array[stack->top--];
+    return '$';
+}
+void push(struct Stack *stack, char op)
+{
+    stack->array[++stack->top] = op;
+}
