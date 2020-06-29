b03ac63b1f8959c6c0369b56e0408d4114338b34
VivanVatsa <VivanVatsa@users.noreply.github.com>
2020-06-29 18:08:00 +0530

    Create StackReverse.cpp

---
+++ stack/C++-Stack/StackReverse.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+#include <stack>
+#include <string>
+using namespace std;
+
+stack<char> st;
+
+// string implementation to store the reversed array
+string ns;
+
+char insert_at_bottom(char x){
+    if(st.size() == 0)
+        st.push(x);
+    else
+    {
+        /* code */
+    }
+    
+}
