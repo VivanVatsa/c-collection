802f37a8ed75aa4dde99800969469db043b83252
VivanVatsa <VivanVatsa@users.noreply.github.com>
2020-07-01 22:21:42 +0530

    Update magiciansalgo.cpp

--- C++STL/algorithms/magiciansalgo.cpp
+++ C++STL/algorithms/magiciansalgo.cpp
@@ -1,21 +1,29 @@
-// all operations in VECTOR
+/*
+ Non-Manipulating Algorithms
 
-// non manipulating algorithm
+    sort(first_iterator, last_iterator) – To sort the given vector.
+    reverse(first_iterator, last_iterator) – To reverse a vector.
+    *max_element (first_iterator, last_iterator) – To find the maximum element of a vector.
+    *min_element (first_iterator, last_iterator) – To find the minimum element of a vector.
+    accumulate(first_iterator, last_iterator, initial value of sum) – Does the summation of vector elements 
+    count(first_iterator, last_iterator,x) – To count the occurrences of x in vector.
+find(first_iterator, last_iterator, x) – Points to last address of vector ((name_of_vector).end()) if element is not present in vector. 
+binary_search(first_iterator, last_iterator, x) – Tests whether x exists in sorted vector or not.
+lower_bound(first_iterator, last_iterator, x) – returns an iterator pointing to the first element in the range [first,last) which has a value not less than ‘x’.
+upper_bound(first_iterator, last_iterator, x) – returns an iterator pointing to the first element in the range [first,last) which has a value greater than ‘x’. 
+
+Some Manipulating Algorithms
+
+    arr.erase(position to be deleted) – This erases selected element in vector and shifts and resizes the vector elements accordingly.
+    arr.erase(unique(arr.begin(),arr.end()),arr.end()) – This erases the duplicate occurrences in sorted vector in a single line. 
+    next_permutation(first_iterator, last_iterator) – This modified the vector to its next permutation.
+prev_permutation(first_iterator, last_iterator) – This modified the vector to its previous permutation. 
+distance(first_iterator,desired_position) – It returns the distance of desired position from the first iterator.This function is very useful while finding the index. 
+*/
 
 #include <bits/stdc++.h>
-#include <iostream>
+#include <algorithm>
 #include <vector>
-#include <numeric>
 
 using namespace std;
-
-int main(){
-
-    // init vector with array
-    int ar[] = {10, 20, 5, 23, 42, 15};
-    int n = sizeof(ar) / sizeof(ar[0]);
-    vector<int> vect(ar, ar + n);
-
-    cout << "Vector is: ";
-}
 
