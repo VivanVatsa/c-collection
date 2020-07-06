597d2c391683fdf117ee44e35dd530ae3d4cef36
VivanVatsa <VivanVatsa@users.noreply.github.com>
2020-07-06 17:23:21 +0530

    Archived Code

---
+++ Algorithms/partitionSTL.cpp
@@ -0,0 +1,12 @@
+// #include <bits/stdc++.h>
+// #include <algorithm>
+// #include <vector>
+
+// using namespace std;
+// int main(void){
+//     vector<int> vect
+// }
+
+// this is a very failed and irrelevant idea to implement the partition STL in C++
+// will look into something else 
+// this is an archived code
