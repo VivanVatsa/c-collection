421309f611115f011eb834006dd3a25f9c574a3e
VivanVatsa <VivanVatsa@users.noreply.github.com>
2020-07-20 21:05:50 +0530

    Update StackReverse.cpp

--- stack/C++-Stack/StackReverse.cpp
+++ stack/C++-Stack/StackReverse.cpp
@@ -5,6 +5,20 @@
 // with using the STL
 using namespace std;
 
+stack<int> st;
+string s;
+// recursive function that inserts elements in the bottom of the stack
+char insertAtBottom(char x){
+    if(st.size() == 0)
+        st.push(x);
+    else
+    {
+        char a = st.top();
+        st.pop();
+        insertAtBottom(x);
+        st.push(a);
+    }
+}
 int main(void){
-    
+
 }
