a077166aeeae1e6a27ba56972c662d2adebbb68e
VivanVatsa <VivanVatsa@users.noreply.github.com>
2020-07-20 13:44:29 +0530

    Update basics.cpp

--- fileHandling/basics.cpp
+++ fileHandling/basics.cpp
@@ -1,1 +1,15 @@
 // extension of the file is C++ but i will try the basics in C
+// reading a file
+#include <bits/stdc++.h>
+#include <stdio.h>
+
+using namespace std;
+
+int main(void){
+    FILE *filepointer;
+    filepointer = fopen("filename.txt", "r");
+    fscanf(filepointer, "%s %s %s %d", str1, str2, str3, &year);
+
+    FILE *filepointer1;
+    filepointer1
+}
