fcaab64cdef3151a52e62c5a0b022ddccbcecb9c
VivanVatsa <VivanVatsa@users.noreply.github.com>
2020-07-23 22:31:18 +0530

    implementing Priority Q using STL

---
+++ Queues/priorityQSTL.cpp
@@ -0,0 +1,45 @@
+// priority queue
+// Priority Queue is an extension of queue with following properties.
+
+//     Every item has a priority associated with it.An element with high priority is dequeued before an element with low priority.If two elements have the same priority,
+//     they are served according to their order in the queue.
+
+// A typical priority queue supports following operations.
+// insert(item, priority): Inserts an item with given priority.
+// getHighestPriority(): Returns the highest priority item.
+// deleteHighestPriority(): Removes the highest priority item.
+
+#include <bits/stdc++.h>
+#include <algorithm>
+#include <queue>
+using namespace std;
+
+void showpq(priority_queue<int> gq){
+    priority_queue<int> g = gq;
+    while(!g.empty()){
+        cout << "\t" << g.top();
+        g.pop();
+    }
+    cout << "\n";
+}
+
+int main(void){
+    priority_queue<int> PQ;
+    PQ.push(10);
+    PQ.push(2);
+    PQ.push(56);
+    PQ.push(12);
+    PQ.push(4);
+    PQ.push(99);
+    PQ.push(1);
+
+    cout << "your priority Queue is: ";
+    showpq(PQ);
+    cout << "\n PQ.size() is: " << PQ.size();
+    cout << "\nPQ.top() element is: " << PQ.top();
+    cout << "\n pq.Pop()";
+    PQ.pop();
+    showpq(PQ);
+
+    return 0;
+}
