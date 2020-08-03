9f452780d45ffa0521609eac9bda6500d6a9eba6
VivanVatsa <VivanVatsa@users.noreply.github.com>
2020-08-03 22:36:51 +0530

    Create orderTree.cpp

---
+++ Trees/traversals/orderTree.cpp
@@ -0,0 +1,6 @@
+/*
+Algorithm Inorder(tree)
+   1. Traverse the left subtree, i.e., call Inorder(left-subtree)
+   2. Visit the root.
+   3. Traverse the right subtree, i.e., call Inorder(right-subtree)
+*/
