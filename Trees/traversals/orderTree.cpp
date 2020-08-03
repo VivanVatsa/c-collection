cb71814f23c9fe56a5ee01ad21dc23e402990aca
VivanVatsa <VivanVatsa@users.noreply.github.com>
2020-08-03 22:37:59 +0530

    Update orderTree.cpp

--- Trees/traversals/orderTree.cpp
+++ Trees/traversals/orderTree.cpp
@@ -4,3 +4,16 @@ Algorithm Inorder(tree)
    2. Visit the root.
    3. Traverse the right subtree, i.e., call Inorder(right-subtree)
 */
+
+/*
+
+
+Depth First Traversals:
+(a) Inorder (Left, Root, Right) : 4 2 5 1 3
+(b) Preorder (Root, Left, Right) : 1 2 4 5 3
+(c) Postorder (Left, Right, Root) : 4 5 2 3 1
+
+Breadth First or Level Order Traversal : 1 2 3 4 5
+
+
+*/
