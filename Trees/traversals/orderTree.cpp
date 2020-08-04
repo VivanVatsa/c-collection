9d0d856390207d7393e1657d1e4710e6b563caba
VivanVatsa <VivanVatsa@users.noreply.github.com>
2020-08-04 12:37:37 +0530

    Update orderTree.cpp

--- Trees/traversals/orderTree.cpp
+++ Trees/traversals/orderTree.cpp
@@ -6,14 +6,10 @@ Algorithm Inorder(tree)
 */
 
 /*
-
-
 Depth First Traversals:
 (a) Inorder (Left, Root, Right) : 4 2 5 1 3
 (b) Preorder (Root, Left, Right) : 1 2 4 5 3
 (c) Postorder (Left, Right, Root) : 4 5 2 3 1
 
 Breadth First or Level Order Traversal : 1 2 3 4 5
-
-
 */
