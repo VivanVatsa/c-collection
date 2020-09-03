c7e72d22000954fe9c14389fe71e01740aafe9f5
Vivan <vivanvatsa@gmail.com>
2020-09-03 11:45:04 +0530

    Update arraymatrixsort.cpp

--- arrays/1D/arraymatrix/arraymatrixsort.cpp
+++ arrays/1D/arraymatrix/arraymatrixsort.cpp
@@ -41,20 +41,20 @@ void dispMatrix(int mat[SIZE][SIZE], int n){
 }
 int main(){
 
-    int mat[SIZE][SIZE] = { { 5, 4, 7 }, 
-                            { 1, 3, 8 }, 
-                            { 2, 9, 6 } }; 
-    int n = 3; 
-  
-    cout << "Original Matrix:\n"; 
-    dispMatrix(mat, n); 
-  
-    sortMatrix(mat, n); 
-  
-    cout << "\nMatrix After Sorting:\n"; 
-    dispMatrix(mat, n); 
-  
-    return 0; 
+    int mat[SIZE][SIZE] = { { 5, 4, 7 },
+                            { 1, 3, 8 },
+                            { 2, 9, 6 } };
+    int n = 3;
+
+    cout << "Original Matrix:\n";
+    dispMatrix(mat, n); {}
+()
+    sortMatrix(mat, n);
+
+    cout << "\nMatrix After Sorting:\n";
+    dispMatrix(mat, n);
+
+    return 0;
 
 }
 
