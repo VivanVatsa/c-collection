22fc3be3c6ab0c622d67bdd8b74957e086b794a4
Vivan <vivanvatsa@gmail.com>
2020-09-07 15:06:14 +0530

    Update BSTPre.cpp

---
+++ Trees/traversals/BSTPre.cpp
@@ -0,0 +1,23 @@
+// aim is to convert BST from PreOrder traversal
+/*
+to construct a tree 
+follow some defined concept
+1. init the nodes that need to be used in the tree
+2. init the function that is going to create a node
+3. 
+
+*/
+#include <bits/stdc++.h>
+#include <stdio.h>
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+class node
+{
+public:
+    int data;
+    node *left;
+    node *right;
+};
