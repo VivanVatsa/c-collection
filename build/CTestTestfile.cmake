8e37f1e5fa675e47ef9cbfa437b08075a298c5c8
Vivan <vivanvatsa@gmail.com>
2020-10-22 17:30:16 +0530

    cmake

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/Vivan/Documents/GitHub/c-collection
+# Build directory: C:/Users/Vivan/Documents/GitHub/c-collection/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
