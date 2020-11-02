53da9d128770af389c67b43da120799ef7b5927f
Vivan <vivanvatsa@gmail.com>
2020-11-02 23:04:20 +0530

    Create BST-C.cpp

---
+++ Trees/Binary Search Trees/BST-C.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <conio.h>
+#include <stdlib.h>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct node
+{
+    int data;
+    struct node *left;
+    struct node *right;
+}
