27c7de326d5ede8ff581e58a643ab8580d97162c
Vivan <vivanvatsa@gmail.com>
2020-11-11 16:47:32 +0530

    BST in C

--- Trees/Binary Search Trees/BST-C.cpp
+++ Trees/Binary Search Trees/BST-C.cpp
@@ -7,18 +7,57 @@ using namespace std;
 
 struct node
 {
-	int data;
+	int key;
 	struct node *left;
 	struct node *right;
-}
+};
 
 // different function to create a new node
 
-struct node *newNode(int item)
+struct node *newNode(int key)
 {
 
 	struct node *temp = (struct node *)malloc(sizeof(struct node));
-	temp->data = item;
+	temp->key = key;
 	temp->left = temp->right = NULL;
 	return temp;
+}
+
+void inorder(struct node *root) {
+
+	if (root != NULL) {
+		inorder(root->left);
+		printf("%d\n", root->key);
+		inorder(root->right);
+	}
+}
+
+struct node *insert(struct node *node, int key) {
+	// checking for different condn here
+	// first condn checks whether the tree is empty. if it is then assign a newnode with this properties
+	if (node == NULL)
+		return newNode(key);
+
+	// if its full then we have to check
+	if (node->key > key)
+		node->left = insert(node->left, key);
+	else if (node->key < key)
+		node->right = insert(node->right, key);
+
+	return node;
+}
+
+int main(void) {
+	struct node *root = NULL;
+	root = insert(root, 50);
+	insert(root, 30);
+	insert(root, 20);
+	insert(root, 40);
+	insert(root, 70);
+	insert(root, 60);
+	insert(root, 80);
+
+	inorder(root);
+
+	return 0;
 }
