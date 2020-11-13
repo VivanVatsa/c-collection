0d19848a6fc6efd92434a581d1057de9fa8634d7
Vivan <vivanvatsa@gmail.com>
2020-11-13 17:39:56 +0530

    few edits

--- Heaps/heap_intro.cpp
+++ Heaps/heap_intro.cpp
@@ -1,8 +1,22 @@
 // binary heap
 #include <bits/stdc++.h>
 using namespace std;
+// #define MAX 500
+// #define f(x) x+x^2
+
+struct node {
+	int data;
+	struct node *left;
+	struct node *right;
+};
+
+struct node *newNode(int key) {
+
+	struct node *temp = (struct node*)malloc(sizeof(struct node));
+
+}
 
 int main(void)
 {
-    int x;
+	int x;
 }
