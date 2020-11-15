7f554f1975a0db38167d8f470c23c3898cd6c231
Vivan <vivanvatsa@gmail.com>
2020-11-15 22:06:52 +0530

    edits

---
+++ Heaps/heap.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <climits>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void swap(int *x, int *y);
+
+class MinHeap {
+	int *harr; // pointer for array of elements in the heap
+	int capacity; // max possible size of heap used as the end element of the heap
+	int heap_size; // current number of elements in min heap
+public:
+	// default constructor
+	MinHeap(int capacity);
+
+	//
+	void MinHeapify(int );
+
+	int parent(int i) {return (i - 1) / 2; }
+}
